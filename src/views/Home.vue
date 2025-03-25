7975da0c9318449a559f0c523a798e1a46aa1721
Francisco Lizcano <flizcanor@icloud.com>
2025-03-24 23:24:43 -0500

    configuración inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,9 @@
             .content-wrapper.px-lg-4.pt-lg-4.px-xxl-5.py-xxl-5.text-start
               p.home-main-title UNIDAD {{ `${globalData.numeroUnidad}` }}
               h1.home-title.mb-4 {{ globalData.tituloUnidad }}
-              p.descripcion.mb-4 Desde los comienzos de la humanidad, la relación entre los principios éticos, el desarrollo económico y la sostenibilidad ha sido fundamental para garantizar un progreso equilibrado y equitativo. En un contexto globalizado, donde los desafíos éticos y ambientales son cada vez más evidentes, la bioética emerge como una herramienta clave para orientar decisiones económicas responsables que respeten tanto el medio ambiente como los derechos humanos.
+              p.descripcion.mb-4 Desde la necesidad de coordinar múltiples áreas operativas, los sistemas integrados de gestión han emergido como una estrategia esencial para el éxito empresarial. Esta unidad profundizará en los conceptos fundamentales, evolución y ventajas de integrar sistemas de gestión como calidad, medio ambiente y seguridad. Con un enfoque práctico, aprenderás cómo estas herramientas optimizan recursos, promueven la sostenibilidad y fortalecen la competitividad organizacional.
 
-              p.descripcion.mb-4 En esta unidad, profundizaremos en el rol de la bioética como pilar de la sostenibilidad en el desarrollo económico, explorando temas esenciales como la aplicación de principios éticos al uso de recursos naturales, la influencia de las políticas globales en la protección de los derechos humanos y la ética en la innovación tecnológica. 
-              
-              p #[b Prepárate para explorar los retos y oportunidades que la sostenibilidad y la bioética presentan en un mundo en constante evolución.]
+              p #[b ¡Prepárese para liderar procesos innovadores con visión estratégica y eficiencia!]
 
               .button-container.mt-auto
                 router-link.btn-iniciar.w-100.w-md-auto(:to="{ name: 'introduccion' }")
@@ -74,7 +72,7 @@ export default {
   .home-main-title
     font-size: 18px
     font-weight: bold
-    color: $color-acento-contenido
+    color: $color-acento-botones
   .home-content
     background-color: white
   .info-section
